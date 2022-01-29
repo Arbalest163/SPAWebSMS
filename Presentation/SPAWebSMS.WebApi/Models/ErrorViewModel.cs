9383bfec12d3c11822b2bebadd8ef376f30fd7e6
Arbalest163 <evil_xacker@mail.ru>
2022-01-29 21:04:59 +0400

    Добавьте файлы проекта.

---
+++ Presentation/SPAWebSMS.WebApi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SPAWebSMS.WebApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
